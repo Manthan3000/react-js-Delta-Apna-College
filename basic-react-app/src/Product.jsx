2263f8a00391297ecbb2611be5ed83f85f8e91d9
Manthan3000 <manthanovhal695@gmail.com>
2024-08-18 17:48:00 +0530

    Reactjs

--- basic-react-app/src/Product.jsx
+++ basic-react-app/src/Product.jsx
@@ -4,12 +4,51 @@ import "./Product.css"
 //Set Default Value For Price 100
 function Product({title,price = 100,features }) {
     
-    return (
+    //map methode
+    // const list = features.map((el) => <li>{el}</li>);
+
+    //Condition on price :
+
+    //1 st Methode 
+
+    // if (price > 40000) {
+    //     return (
+    //         <div className="Product">
+    //             <h3>{title}</h3>
+    //             <p>{price}$</p>
+    //             <p>Discount 5%  </p>
+
+    //             {/* <p>{list}</p> */}
+    //             {/* <p>{features.map((el) => <li>{el}</li>) }</p> */}
+            
+    //         </div>
+    //     )
+    // } else {
+    //     return (
+    //         <div className="Product">
+    //             <h3>{title}</h3>
+    //             <p>{price}$</p>
+
+    //             {/* <p>{list}</p> */}
+    //             {/* <p>{features.map((el) => <li>{el}</li>) }</p> */}   
+    //         </div>
+
+    //     )
+    // }
+    
+    // Methode 2
+    // let isDiscount = price > 30000 ? "5% Off" : "";
+    
+        return (
         <div className="Product">
             <h3>{title}</h3>
             <p>{price}</p>
-            <p>{features}</p>
-            
+                
+            {/* <p>{isDiscount }</p> */}
+            {price > 30000 ? <p> 5% Off </p> : null}
+                
+            {/* <p>{list}</p> */}
+            {/* <p>{features.map((el) => <li>{el}</li>) }</p> */}
         </div>
 
     )
--- basic-react-app/src/ProductTab.jsx
+++ basic-react-app/src/ProductTab.jsx
@@ -3,17 +3,25 @@ import Product from "./Product";
 //Default Arg for Price is 100
 
 function ProductTab() {
+    //  Send array in Form of List Methode 1 :
+    let options = ["hi-tech ","Durable " ,"Fast "];
 
-    let options = ["hi-tech ", "Durable ", "Fast ", "Usefull "];
+    //  Send array in form of List Methode 2 :
+    //  MOST IMP map() Function methode
+    //map methode ===
 
+    // const list = features.map((el) => <li>{el}</li>);
+
+
+    //Send Objects
     // let option2 = {a: "Hi-tech",b:"Durable",c:"Fast",d:"Usefull"}
     
     return (
         <>
             <Product title="Laptop" price={90000} features = {options} />
-            <Product title="Phone" price={35000} features = {["hi-tech ", "Durable ", "Fast ", "Usefull "]}/>
-            <Product title="Charger"  features = {["hi-tech ", "Durable ", "Fast ", "Usefull "]}/>
-            <Product title="Tab" price={45000}  features = {["hi-tech ", "Durable ", "Fast ", "Usefull "]} />
+            <Product title="Phone" price={23000} features = {["hi-tech ", "Durable ", "Fast ", "Usefull "]}/>
+            <Product title="Charger" features = {options}/>
+            <Product title="Tab" price={35000}  features = {options} />
             
         </>
     )
